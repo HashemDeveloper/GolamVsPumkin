e6a119d3a3728f4382631a392fa267598a245c2f
Abul Hashem Jr <abul@limosys.com>
2019-07-17 21:02:35 -0400

    Golam vs Pumkin

---
+++ Assets/Scripts/CameraFollowPumkin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPumkin : MonoBehaviour
+{
+    private Transform pumkinPosition;
+    private float yDistance = 4f;
+    private float zDistance = 10f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        this.pumkinPosition = GameObject.Find("Pumkin").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        followCamera();
+    }
+    private void followCamera() {
+        Vector3 temPosition = transform.position;
+        temPosition.y = this.pumkinPosition.position.y + this.yDistance;
+        temPosition.z = this.pumkinPosition.position.z - this.zDistance; // minus because we want camera to be behind pumkin
+        // reassign the value of transposition to default
+        transform.position = temPosition;
+    }
+}
---
+++ Assets/Scripts/PumkinScripts.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PumkinScripts : MonoBehaviour
+{
+    public float moveForce = 10.0F;
+    private Rigidbody rigidBody;
+    // Start is called before the first frame update
+
+    /**
+     /// Awake is called when the script instance is being loaded.
+     // this function is gonna be called first before start() method
+     */
+    void Awake()
+    {
+        this.rigidBody = GetComponent<Rigidbody>();
+    }
+    // start() is for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < -20 && transform.position.y < -20) {
+            Time.timeScale = 0.0f;
+        }
+    }
+    /**
+        thie following method FixedUpdate() gets called at the moment of every 15 frame gets executed
+     */
+    void FixedUpdate() {
+        movePumkin();
+    }
+    private void movePumkin() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        // Vector3 represents 3D vectors and points. it moves object in x-axis, y-axis and z-axis
+        // x-axis is move left to right, y-axis is move up and down while z-axis moves back and forward
+        // therefore, Vector3 takes these 3 parameters(x, y, z)
+        this.rigidBody.AddForce(new Vector3(horizontal * moveForce, 0.0F, vertical * moveForce));
+    }
+    
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider target)
+    {
+        if (target.tag == "GOLAM" || target.tag == "GATES") {
+            Time.timeScale = 0f;
+        }
+    }
+}
